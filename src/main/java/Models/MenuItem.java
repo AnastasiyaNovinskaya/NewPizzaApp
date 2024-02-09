f75ad221caf2493c2908c32edbe928f2976876e6
Saule Anafinova~ <saule.anafinova@gmail.com>
2024-02-09 15:00:15 +0100

    pizzaQuantity added

--- src/main/java/Models/MenuItem.java
+++ src/main/java/Models/MenuItem.java
@@ -5,37 +5,30 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Arrays;
 
-
 public class MenuItem {
-    private final PizzaType pizzaType;
-    private final IngredientType ingredientType;
-    private final String selectedPizzaId;
-    private final Connection connection;
+    private Connection connection;
 
-    public MenuItem(Connection connection, PizzaType pizzaType, IngredientType ingredientType, String selectedPizzaId) {
+    public MenuItem(Connection connection) {
         this.connection = connection;
-        this.pizzaType = pizzaType;
-        this.ingredientType = ingredientType;
-        this.selectedPizzaId = selectedPizzaId != null ? selectedPizzaId : "";
     }
 
-    public void addOrder(String pizzaType, double pizzaPrice, String ingredientName, double ingredientPrice) throws SQLException {
+    public void addOrder(String pizzaType, double pizzaPrice, int pizzaQuantity, String ingredientName, double ingredientPrice) throws SQLException {
         // Check if pizzaType is valid
         if (!Arrays.asList("pepperoni", "margherita", "hawaii", "prosciutto", "vegetarian", "seafood").contains(pizzaType)) {
             throw new IllegalArgumentException("Invalid pizza type: " + pizzaType);
         }
 
-        String sendOrder = "INSERT INTO pizza.orders (pizzaType, pizzaPrice, ingredientName, ingredientPrice) VALUES (?, ?, ?, ?)";
+        String sendOrder = "INSERT INTO pizza.orders (pizzaType, pizzaPrice, pizzaQuantity, ingredientName, ingredientPrice) VALUES (?, ?, ?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(sendOrder)) {
             preparedStatement.setString(1, pizzaType);
             preparedStatement.setDouble(2, pizzaPrice);
-            preparedStatement.setString(3, ingredientName);
-            preparedStatement.setDouble(4, ingredientPrice);
+            preparedStatement.setInt(3, pizzaQuantity);
+            preparedStatement.setString(4, ingredientName);
+            preparedStatement.setDouble(5, ingredientPrice);
 
             preparedStatement.executeUpdate();
         } catch (SQLException exception) {
             throw new SQLException("Failed to add order: " + exception.getMessage());
         }
     }
-
 }
